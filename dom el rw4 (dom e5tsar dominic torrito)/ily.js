b753c3ea37bd0951c94eacbc279da47255e9f09a
HaziimYassien <haziim.yassien@gmail.com>
2021-12-13 21:34:36 +0200

    dom torreto

---
+++ dom el rw4 (dom e5tsar dominic torrito)/ily.js
@@ -0,0 +1,13 @@
+//momkn tgiip el id pii tarii2a rw4a aw pii tarii2a arwa4
+
+//el rw4
+document.getElementById("header")
+document.getElementsByClassName("header")
+    //keda enta 7addt enk 3ayz t5tar id aw class
+
+//el tarii2a el arwa4
+document.querySelector("#header")
+document.querySelector(".header")
+    //bil querySelector momkn t5ar id aw class 3la 7asap hat7ot eah 2apl el kelma
+    //. m3anha ana keda ha5tar class
+    //# m3anha ana keda ha5tar id
